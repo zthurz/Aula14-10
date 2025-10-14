0bac1289cc3e32459319ac79727c7abf552ae267
zthurz <zthurzzz@gmail.com>
2025-10-14 20:40:24 -0300

    licao de casa

--- Aula.py
+++ Aula.py
@@ -1,18 +1,24 @@
-print("Ola, somos da equipe estacio e estamos atualizando o seu cadastro de aluno, por favor nos de algumas informações", "\n")
 Nome = input("Qual é o seu nome completo: ")
+
+
+
+
+
+
+
 Idade = int(input("Qual é a sua idade: "))
-Telefone = (input("Qual é o seu numero de telefone? ex: (00) 123456789: "))
-Endereço = input("Qual é o seu endereço: ")
-Documento = int(input("Agora eu preciso de um documento seu, lembre-se apenas numeros: "))
 
-print("Certinho, obrgiado por atualizar seus dados", "\n","\n")
+Informacoes = input("Voce quer que eu te mostre suas informaçoes? (Responda sim ou nao) ")
+if Informacoes == "nao":
+        print("Ok, obrigado por atualizar seu cadastro")
 
-print("O seu nome é", Nome, "\n")
+elif Informacoes == "sim":
+    
 
-print("Voce tem", Idade, "Anos", "\n")
+    print("Certinho, obrgiado por atualizar seus dados", "\n","\n")
 
-print("Seu numero de telefone é", Telefone, "\n")
+    print("O seu nome é", Nome, "\n")
 
-print("Voce mora em", Endereço, "\n")
+    print("Voce tem", Idade, "Anos", "\n")
 
-print("E por ultimo, seu documento é", Documento, "\n")
+    
